8800a3d8588cff145e7e8d21b2f5533fdd5d4aed
Ali Kerem Kaya <alikeremkayaa@gmail.com>
2025-02-03 16:15:47 +0300

    1Lesson

---
+++ UnitTestCourse.Introduction/E01UnitTest.cs
@@ -0,0 +1,50 @@
+﻿namespace UnitTestCourse.Introduction
+{
+    // This is a test method that checks if the sum of two numbers is correct
+    public class E01UnitTest
+    {
+        
+
+        //En küçük parçayı test etmek için kullanılır.
+
+        //Bir metotun doğru çalışıp çalışmadığını test etmek için kullanılır ve metotlarımızın doğru çalıştığını garanti altına alır.
+
+        //Ek olarak testlerimizdeki hataların nerede olduğunu bulmamızı sağlar.
+
+
+
+        //Neden Unit Test Yazmalıyız?
+
+        //Sistemin tasarladığı gibi çalışıp çalışmadığını kontrol etmek için,
+
+        //Sistemin beklenen sonuçları verip vermediğini kontrol etmek için,
+
+        //Kod kalitemizi arttırmak için,(bug-fix loop, reusability,DI)
+
+        //Kodlarımızda değişiklik yapma hızımızı arttırmak için,
+
+
+    }
+    //Unit test nasıl yazılır?
+    //1. Arrange: Testin başarılı olması için gerekli olan koşulların oluşturulması
+    //2. Act: Test edilecek metotun çağrılması
+    //3. Assert: Metot çağrıldıktan sonra dönmesi gereken değerlerin kontrol edilmesi
+    //4. CleanUp: Test sonrası gereken temizlik işlemlerinin yapılması
+    //5. Test metotları genellikle void tipinde olur ve geriye bir değer döndürmezler.
+    //6. Test metotları genellikle geriye bool tipinde bir değer döndürürler. Bu değer testin başarılı olup olmadığını belirtir.
+    //7. Test metotları genellikle Test adı ile başlar.
+    //8. Hata sonuçları anlaşılabilir olmalı, yönlendirilmeli ve açık olmalıdır.
+
+
+    //Terminoloji
+
+    //Unit of Work: Test edilecek olan metot 
+    //Test Fixture: Test edilecek olan metotların bulunduğu sınıf
+    //Test Case: Test edilecek olan metotun bir test durumu
+    //Test Suite: Birbirleriyle ilişkili testlerin bir araya getirilmesi
+    //Unit of Work-Çalışma Alanımız
+    //Unit Test
+    //Assert
+    //Arrange-Action-Assertion Blocks
+    //Mock-Moq
+}
